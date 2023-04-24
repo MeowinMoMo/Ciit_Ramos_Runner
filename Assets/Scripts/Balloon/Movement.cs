92e7a72d8a647baa786c5599759704d045a9f74f
MeowinMoMo <OTAKU4ever29@gmail.com>
2023-04-24 18:27:43 +0800

    few changes

---
+++ Assets/Scripts/Balloon/Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Speed;
+    public float SideSpeed;
+
+    void Start() {
+
+    }
+    // Update is called once per frame
+    void Update() {
+        transform.position += Vector3.forward * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position += Vector3.right * SideSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position += Vector3.left * SideSpeed * Time.deltaTime;
+        }
+    }
+}
